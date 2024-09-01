9f4db52b448ed36b1bc007f70acc364b1a917f2e
MIJanda <mokiliisaacjanda@gmail.com>
2024-09-01 04:30:26 +0300

    feat(custom fonts): add imports

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,8 @@
 import { Stack } from "expo-router";
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
 
 export default function RootLayout() {
   return (
