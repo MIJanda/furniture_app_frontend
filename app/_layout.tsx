f450372304a460601a97babcfe40af99e67dc156
MIJanda <mokiliisaacjanda@gmail.com>
2024-09-01 05:04:39 +0300

    feat(custom fonts): complete custom font loading

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,30 @@
 import { Stack } from "expo-router";
-import { StatusBar } from 'expo-status-bar';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useCallback } from 'react';
 
 export default function RootLayout() {
+
+  // load fonts
+  const [fontsLoaded] = useFonts({
+    pBold: require("@/assets/fonts/Poppins-Bold.ttf"),
+    pExtraBold: require("@/assets/fonts/Poppins-ExtraBold.ttf"),
+    pLight: require("@/assets/fonts/Poppins-Light.ttf"),
+    pMedium: require("@/assets/fonts/Poppins-Medium.ttf"),
+    pRegular: require("@/assets/fonts/Poppins-Regular.ttf"),
+    pSemibold: require("@/assets/fonts/Poppins-SemiBold.ttf"),
+  });
+
+  const onRootLayoutView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <Stack>
       <Stack.Screen name="index" />
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { StatusBar } from 'expo-status-bar';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.textStyle}> Edit app/index.tsx to edit this screen.</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle: {
+    fontFamily: 'pMedium',
+    fontSize: 20,
+  },
+});
